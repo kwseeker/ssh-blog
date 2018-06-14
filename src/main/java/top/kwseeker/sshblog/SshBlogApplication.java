07e6a179866593960914346160c3bb40e6ae4d31
Arvin Lee <xiaohuileee@gmail.com>
2018-06-14 12:24:06 +0800

    零配置构建SpringBoot博客项目

---
+++ src/main/java/top/kwseeker/sshblog/SshBlogApplication.java
@@ -0,0 +1,12 @@
+package top.kwseeker.sshblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshBlogApplication.class, args);
+	}
+}
